e76a2fadf0523f5d8bc266058f75baf6d58dcacf
☠️ <93435269+funsimplenimble@users.noreply.github.com>
2025-07-20 14:28:50 -0400

    docs(test): clarify choice of delimiter

--- test.sh
+++ test.sh
@@ -21,5 +21,9 @@ run() {
   echo "Test|Exit|Output"
   run "Invalid Content-Type" -X POST "localhost:${PORT}" -H "Content-Type: application/json" -d '{"foo":"bar"}'
   run "Valid Content-Type"   -X POST "localhost:${PORT}" -H "Content-Type: application/text" -d '{"foo":"bar"}'
-  run "OPTIONS /"            -X OPTIONS "localhost:${PORT}"
+  run "OPTIONS /"   /y         -X OPTIONS "localhost:${PORT}"
 } | column -t -s '|'
+# We use the pipe character '|' as a delimiter for the column command instead of a tab '	'.
+# This is because the output from curl can contain spaces and other characters that might
+# be misinterpreted by the column command when using tabs, leading to formatting issues.
+# The pipe is a safer choice as it's unlikely to appear in the curl output.
